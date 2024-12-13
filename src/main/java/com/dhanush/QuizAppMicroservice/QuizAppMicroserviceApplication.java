2b953cf0b5b2c3dc02bb974dd43e0049aa0b6e59
dhanushgr <grdhanush8@gmail.com>
2024-12-13 18:55:13 +0530

    created QuizAppMicroservice

---
+++ src/main/java/com/dhanush/QuizAppMicroservice/QuizAppMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dhanush.QuizAppMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhanush/QuizAppMicroservice/QuizAppMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhanush.QuizAppMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
